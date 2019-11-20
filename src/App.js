c4361fb90730beb8332c86482f18c615270260a4
Rechie Lagria <41753498+foxching@users.noreply.github.com>
2019-11-20 23:50:43 +0800

    Add navbar component and setup routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import Dashboard from "./components/dashboard/Dashboard";
+import Navbar from "./components/navigation/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <Dashboard />;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Dashboard />
+      </div>
+    </Router>
+  );
 };
 export default App;
---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar navbar-expand-lg navbar-light bg-primary">
+        <div className="container">
+          <Link to="/" className="navbar-brand text-white">
+            MEMES T-SHIRT
+          </Link>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarComponent"
+          />
+          <div className="collapse navbar-collapse" id="navbarComponent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  SIGN UP
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  LOGIN
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
